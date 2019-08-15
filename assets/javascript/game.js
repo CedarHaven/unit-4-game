65896921f9c78d8db2a4b8014d4c090cef2838bc
Cedar <alexianfireflies@gmail.com>
2019-08-14 19:04:07 -0700

    added comments and changed some classes to ids

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,35 @@
+// I'm so tired.
+// I wrote most of this over the course of five-six hours.
+// help me.
 // this is an array of all the character objects.
 // the only reason playerAttack is in here is because the instructions said every character should have a separate enemy attack, player attack, and health variable. at least, I assume they meant "separate variables", because otherwise that instruction would be in direct conflict with the instruction of "it shouldn't be about picking the strongest character", because....different player attack variables...inherently means....there is a strongest character...who is more likely to win.
 // basically, if they meant "all the characters should have different player attack numbers", then I recognize I was given a direction, but given that it's a stupid direction, I have elected to ignore it. /approximate movie quotes yay.
+// I swear I can be more professional in my comments.
+// but holy fuck I'm so tired right now.
+// I wouldn't do it if I was handing this right to potential employers or something.
+// I realized very belatedly that my Captain America image is from Endgame, and that he doesn't normally have a hammer, but I am beyond caring.
 var characters = [ 
     {name: "Hawkeye",
     health: 350,
-    playerAttack: 10,
+    playerAttack: 12,
     imgPath: "assets/images/hawkeye.jpg",
     enemyAttack: 30},
 
     {name: "Black Widow",
     health: 370,
-    playerAttack: 10,
+    playerAttack: 12,
     imgPath: "assets/images/black-widow.jpg",
     enemyAttack: 20},
 
     {name: "Captain America",
     health: 410,
-    playerAttack: 10,
+    playerAttack: 12,
     imgPath: "assets/images/cap.jpg",
     enemyAttack: 35},
 
     {name: "Iron Man",
     health: 390,
-    playerAttack: 10,
+    playerAttack: 12,
     imgPath: "assets/images/iron-man.jpg",
     enemyAttack: 40}
 ]
@@ -39,7 +46,7 @@ var defenderChar = {};
 var win = 0;
 var defender = false;
 var youLive = true;
-var yourAttack = 10;
+var yourAttack = 12;
 var yourHealth = 0;
 var enemyHealth = 0;
 var resetButton = $("<button/>", {
@@ -72,7 +79,7 @@ function createCharacter() {
         $(charCard).append(charName);
         $(charCard).append(charImage);
         $(charCard).append(charHealth);
-        $(".character-selection").append(charCard);
+        $("#character-selection").append(charCard);
     }
 }
 
@@ -84,7 +91,9 @@ function appendEnemy() {
         charName.text(enemyChars[i].name);
         charHealth.text(enemyChars[i].health);
 
-        // I added the id to enemy character cards because I couldn't figure out how to remove them when they're selected as defenders without the id, and they do need to be removed from the enemy selection area when you choose them as a defender. I also removed the space from the name, because otherwise I couldn't select the id for things like "Black Widow".
+        // I added the id to enemy character cards because I couldn't figure out how to remove them when they're selected as defenders without the id, and they do need to be removed from the enemy selection area when you choose them as a defender. 
+        // I also removed the space from the name, because otherwise I couldn't select the id for things like "Black Widow".
+        // I am admittedly unclear why it's outlining "/\/s/" in red, given that it works fine and this IS what was suggested to me, but. whatever. I guess.
         var id = enemyChars[i].name;
         id = id.replace(/\s/g, '');
         var charCard = $(`<div class = 'enemy-card' data-name = "${enemyChars[i].name}">`);
@@ -92,52 +101,52 @@ function appendEnemy() {
         $(charCard).append(charName);
         $(charCard).append(charImage);
         $(charCard).append(charHealth);
-        $(".enemy-characters").append(charCard);
+        $("#enemy-characters").append(charCard);
     }
 }
 
 function appendPlayer(){
     var charName = $("<div/>");
     var charImage = $("<img src = "+playerChar.imgPath+">");
-    var charHealth = $("<div class = 'player-health'/>");
+    var charHealth = $("<div id = 'player-health'/>");
     charName.text(playerChar.name);
     charHealth.text(playerChar.health);
-    var charCard = $(`<div class = 'player-card' data-name = "${playerChar.name}">`);
+    var charCard = $(`<div id = 'player-card' data-name = "${playerChar.name}">`);
     $(charCard).append(charName);
     $(charCard).append(charImage);
     $(charCard).append(charHealth);
-    $(".player-character").append(charCard);
+    $("#player-character").append(charCard);
 }
 
 function appendDefender(){
     var charName = $("<div/>");
     var charImage = $("<img src = "+defenderChar.imgPath+">");
-    var charHealth = $("<div class = 'defender-health'/>");
+    var charHealth = $("<div id = 'defender-health'/>");
     charName.text(defenderChar.name);
     charHealth.text(defenderChar.health);
-    var charCard = $(`<div class = 'defender-card' data-name = "${defenderChar.name}">`);
+    var charCard = $(`<div id = 'defender-card' data-name = "${defenderChar.name}">`);
     $(charCard).append(charName);
     $(charCard).append(charImage);
     $(charCard).append(charHealth);
-    $(".current-enemy").append(charCard);
+    $("#current-enemy").append(charCard);
 }
 
 function defHealthUpdate() {
-    $(".defender-health").html(enemyHealth);
+    $("#defender-health").html(enemyHealth);
 }
 
 function playerHealthUpdate() {
-    $(".player-health").html(yourHealth);
+    $("#player-health").html(yourHealth);
 }
 
-$(".character-selection").on("click", ".char-card", function(e) {
+$("#character-selection").on("click", ".char-card", function(e) {
     
     // this goes looking for the customized data-name attribute of what you clicked on.
     var charSelect = $(this).attr("data-name");
 
-    // this is the charSelect thing. it determines who you've chosen as your player, empties the selection div, sets your player charater object, sets your current health, sets the array of enemy characters, and then appends the enemy characters to the appropriate div and the player character to the appropriate div.
+    // this is the charSelect thing. it determines who you've chosen as your player, empties the selection div, sets your player charater object, sets your current health, sets the array of enemy characters, and then calls the functions that append the enemy characters to the appropriate div and the player character to the appropriate div.
     if(charSelect === "Hawkeye") {
-        $(".character-selection").empty();
+        $("#character-selection").empty();
 
         for(var i = 0; i < characters.length; i++){
             if(characters[i].name != "Hawkeye"){
@@ -153,7 +162,7 @@ $(".character-selection").on("click", ".char-card", function(e) {
         appendEnemy();
     }
     else if(charSelect === "Black Widow") {
-        $(".character-selection").empty();
+        $("#character-selection").empty();
 
         for(var i = 0; i < characters.length; i++){
             if(characters[i].name != "Black Widow"){
@@ -169,7 +178,7 @@ $(".character-selection").on("click", ".char-card", function(e) {
         appendEnemy();
     }
     else if(charSelect === "Captain America") {
-        $(".character-selection").empty();
+        $("#character-selection").empty();
 
         for(var i = 0; i < characters.length; i++){
             if(characters[i].name != "Captain America"){
@@ -185,7 +194,7 @@ $(".character-selection").on("click", ".char-card", function(e) {
         appendEnemy();
     }
     else if(charSelect === "Iron Man") {
-        $(".character-selection").empty();
+        $("#character-selection").empty();
 
         for(var i = 0; i < characters.length; i++){
             if(characters[i].name != "Iron Man"){
@@ -202,7 +211,7 @@ $(".character-selection").on("click", ".char-card", function(e) {
     }
 });
 
-$(".enemy-characters").on("click", ".enemy-card", function(e) {
+$("#enemy-characters").on("click", ".enemy-card", function(e) {
     if(!defender) {
         var defSelect = $(this).attr("data-name");
 
@@ -215,6 +224,7 @@ $(".enemy-characters").on("click", ".enemy-card", function(e) {
                 }
             }
 
+            enemyHealth = defenderChar.health;
             $("#Hawkeye").remove();
             appendDefender();
         }
@@ -227,6 +237,7 @@ $(".enemy-characters").on("click", ".enemy-card", function(e) {
                 }
             }
 
+            enemyHealth = defenderChar.health;
             $("#BlackWidow").remove();
             appendDefender();
         }
@@ -239,6 +250,7 @@ $(".enemy-characters").on("click", ".enemy-card", function(e) {
                 }
             }
 
+            enemyHealth = defenderChar.health;
             $("#CaptainAmerica").remove();
             appendDefender();
         }
@@ -251,6 +263,7 @@ $(".enemy-characters").on("click", ".enemy-card", function(e) {
                 }
             }
 
+            enemyHealth = defenderChar.health;
             $("#IronMan").remove();
             appendDefender();
         }
@@ -259,8 +272,6 @@ $(".enemy-characters").on("click", ".enemy-card", function(e) {
 
 $("#attack-button").click(function(){
     if(defender && youLive){
-        enemyHealth = defenderChar.health;
-
         // you always attack first.
         enemyHealth = enemyHealth-yourAttack;
         defHealthUpdate();
@@ -271,7 +282,7 @@ $("#attack-button").click(function(){
             playerHealthUpdate();
 
             // given that these sentences show up at the exact same time in the demo, I felt okay about putting them here, because it only goes if you and your enemy are able to attack. if they can't attack, they're dead, and you get told you won the fight.
-            $(".attacks").html("You attacked "+defenderChar.name+" for "+yourAttack+" damage.<br>"+defenderChar.name+" attacked you for "+defenderChar.enemyAttack+" damage.");
+            $("#attacks").html("You attacked "+defenderChar.name+" for "+yourAttack+" damage.<br>"+defenderChar.name+" attacked you for "+defenderChar.enemyAttack+" damage.");
             
             // I'm going to be real, I incremented by playerAttack so I could feel like the variable was doing SOMETHING instead of just sitting uselessly.
             yourAttack = yourAttack+playerChar.playerAttack;
@@ -279,26 +290,27 @@ $("#attack-button").click(function(){
 
         // if you're now dead as a result of enemy attack, you are informed you have lost.
         if(yourHealth <= 0) {
-            $(".attacks").html("You have been defeated.<br>Game Over.");
+            $("#attacks").html("You have been defeated.<br>Game Over.");
             youLive = false;
-            $(".reset").append(resetButton);
+            $("#reset").append(resetButton);
         }
         else if(enemyHealth <= 0) {
-            $(".defender-card").remove();
-            $(".attacks").html("You have defeated "+defenderChar.name+"! You may now select another enemy to fight.");
+            $("#defender-card").remove();
+            $("#attacks").html("You have defeated "+defenderChar.name+"! You may now select another enemy to fight.");
             defender = false;
             win++;
         }
     }
 
+    // if you've fought and defeated all available enemies, you win! yay you!
     if(win==3) {
-        $(".attacks").html("You have won!");
-        $(".reset").append(resetButton);
+        $("#attacks").html("You have won!");
+        $("#reset").append(resetButton);
     }
 });
 
 $("#reset-button").click(function(){
-    $(".reset").empty();
+    $("#reset").empty();
     wins = 0;
     createCharacter();
 });
